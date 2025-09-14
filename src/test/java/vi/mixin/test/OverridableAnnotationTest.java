fd01c3095dc7da28bc9497f52bc7e0178d4935d6
vi945786 <violet945786@gmail.com>
2025-09-15 00:51:57 +0300

    Add files via upload

---
+++ src/test/java/vi/mixin/test/OverridableAnnotationTest.java
@@ -0,0 +1,30 @@
+package vi.mixin.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import vi.mixin.api.annotations.Mixin;
+import vi.mixin.api.annotations.classes.Extends;
+import vi.mixin.api.annotations.methods.Overridable;
+
+public class OverridableAnnotationTest {
+
+    public static class Base {
+        private int value;
+        public int getValue(int x) { return value + x; }
+    }
+
+    @Test
+    public void testOverridable() {
+        assertTrue(Base.class.isAssignableFrom(OverridableAnnotationTestExtender.class));
+        assertEquals(15, new OverridableAnnotationTestExtender().getValue(10));
+    }
+}
+
+@Mixin(OverridableAnnotationTest.Base.class) @Extends
+class OverridableAnnotationTestExtender {
+
+    @Overridable("getValue(I)I")
+    public int getValue(int x) {
+        return 15;
+    }
+}
